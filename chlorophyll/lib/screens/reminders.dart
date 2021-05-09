4a5d21489c70356b0a2dd2a9523c8d0770834a53
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-09 19:13:33 +0530

    started reminder screen

--- chlorophyll/lib/screens/reminders.dart
+++ chlorophyll/lib/screens/reminders.dart
@@ -1,3 +1,6 @@
+import 'package:chlorophyll/constants/theme.dart';
+import 'package:chlorophyll/helpers/size.dart';
+import 'package:chlorophyll/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class Reminders extends StatefulWidget {
@@ -8,6 +11,49 @@ class Reminders extends StatefulWidget {
 class _RemindersState extends State<Reminders> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeHelper s = SizeHelper(context);
+    return Stack(
+      children: [
+        Positioned(
+          left: -120,
+          top: -120,
+          child: Container(
+            height: s.hHelper(50),
+            width: s.hHelper(50),
+            decoration: BoxDecoration(
+              color: secondaryGreen,
+              borderRadius: BorderRadius.circular(1000),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: s.wHelper(5),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: s.hHelper(8),
+              ),
+              Text(
+                "Reminders",
+                style: bigTextBold,
+              ),
+              SizedBox(
+                height: s.hHelper(4),
+              ),
+              SizedBox(
+                height: s.hHelper(4),
+              ),
+              CustomButton(
+                title: "Set new reminders",
+                onButtonPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
