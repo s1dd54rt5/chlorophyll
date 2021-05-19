84f62a885ce91483485fbb19f9fd6b5470da93b8
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-19 23:30:43 +0530

    test

--- chlorophyll/test/widget_test.dart
+++ chlorophyll/test/widget_test.dart
@@ -5,25 +5,25 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:chlorophyll/root.dart';
-import 'package:flutter/material.dart';
+import 'package:chlorophyll/widgets/button.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  Widget testWidget = new MediaQuery(
+      data: new MediaQueryData(),
+      child: new MaterialApp(
+          home: new CustomButton(
+        title: "Login",
+        onButtonPressed: () {
+          print("Succesfully Logged in!");
+        },
+      )));
+  testWidgets('Testing Login Button Widget', (WidgetTester tester) async {
+    await tester.pumpWidget(testWidget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.tap(find.widgetWithText(CustomButton, "Login"));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.widgetWithText(CustomButton, "Login"), findsOneWidget);
   });
 }
